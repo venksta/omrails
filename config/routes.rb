0da10ecfa8cf97b2ae7dbdbb08e4b5a3db99ffaf
Venk Seelam <venk2005@gmail.com>
2013-05-30 15:03:12 -0700

    Add User Profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Omrails::Application.routes.draw do
 
+  get "users/show"
+
   resources :pins
 
 
   devise_for :users
+  match 'users/:id' => 'users#show', as: :user
+  
 
 root :to =>'pins#index'
 get 'about' => 'pages#about'
