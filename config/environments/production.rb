21022fad2eaece147ce42df0bdf70bdfdb9fcfc9
Venk Seelam <venk2005@gmail.com>
2013-05-29 20:35:35 -0700

    Connect Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,17 @@ Omrails::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+
+# Configuring Amazon S3 for Paperclip
+config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
+
 end
